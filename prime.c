9b8ee074464ff6ca3487cad5247ac1edfb8449bd
sarthak1930 <SARTHAKSHINDE1930@GMAIL.COM>
2025-05-23 09:08:23 +0530

    Add files via upload

---
+++ prime.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int num,i,rem,check=0;
+    printf("enter the number\t");
+    scanf("%d",&num);
+
+    for(i=2;i<num;i++)
+    {
+        rem=num%i;
+        if(rem==0)
+        {
+            check=1;
+            break;
+        }
+    }
+    if (check==0)
+    {
+        printf("prime");
+
+    }
+    else
+    {
+        printf("not prime");
+    }
+    return 0;
+}
