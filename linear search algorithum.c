f65d7bdfeda7a9d9fb845351a20aa6bf58b357ed
sarthak1930 <SARTHAKSHINDE1930@GMAIL.COM>
2025-06-22 17:12:11 +0530

    Update linear search algorithum.c

--- linear search algorithum.c
+++ linear search algorithum.c
@@ -4,26 +4,29 @@
 //Branch-EXTC
 
 #include<stdio.h>
-
 int linearsearch(int arr[], int size, int value);
 
 int main() {
-    int a[10] = {9, 8, 7, 5, 6, 3, 4, 2, 1}; 
+    int a[10] = {9, 8, 7, 5, 6, 3, 4, 2, 1,0}; 
     int result, key;
     printf("Enter number to find: ");
     scanf("%d", &key);
     result = linearsearch(a, 10, key); 
-    if (result == -1) {
+    if (result == -1)
+    {
         printf("Not found");
-    } else {
+    } else
+    {
         printf("The number %d is found at index %d", key, result);
     }
     return 0;
 }
 int linearsearch(int arr[], int size, int value) 
 {
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == value) {
+    for (int i = 0; i < size; i++)
+        {
+        if (arr[i] == value)
+        {
             return i;
         }
     }
