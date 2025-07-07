05ff19314df728eafdb3c309ba003dd49bb86864
sarthak1930 <SARTHAKSHINDE1930@GMAIL.COM>
2025-07-07 23:54:27 +0530

    Add files via upload

---
+++ PrintNumber.c
@@ -0,0 +1,14 @@
+//Sarthak Tukaram Shinde
+//PRN-24030331372046
+//Branch-EXTC
+
+#include <stdio.h>
+
+int main() {
+    int number;
+    printf("Enter a number: ");
+    scanf("%d", &number);
+    printf("You entered: %d\n", number);
+    
+    return 0;
+}
